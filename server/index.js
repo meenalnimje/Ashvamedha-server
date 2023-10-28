082285dc3e1f38f052eb5cdc2edb47ef52605150
meenal <meenalnimje@gmail.com>
2023-10-28 11:43:05 +0530

    error in updatelive score rectified

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV == "productions") {
       res.setHeader("Access-Control-Allow-Origin", origin);
     }
     //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-    res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.header("Access-Control-Allow-Methods", " POST, PUT, GET, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", true);
     return next();
